750c037aaf6b3c2fdb7ba3699196e337dd3855ee
jiaming <743192023@qq.com>
2018-12-05 18:31:43 +0800

    some modfiy

---
+++ src/components/numberShow/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="number-show">
+    <div class="number-itme" v-for="n in number.toString()" :key="n">
+      {{ n }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'NumberShow',
+  props: ['number']
+}
+</script>
+
+<style lang="less">
+.number-show {
+  display: inline-block;
+
+  .number-itme {
+    display: inline-block;
+    height: 70px;
+    padding: 0 20px;
+    line-height: 70px;
+    text-align: center;
+    background-color: rgba(4, 49, 128, 0.6);
+    margin-right: 20px;
+    color: rgb(8, 229, 255);
+    font-weight: bold;
+    font-size: 45px;
+  }
+}
+</style>
