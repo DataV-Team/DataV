ec02217c9f09d0edbcaa737f4cf3a21b349d1406
jiaming <743192023@qq.com>
2018-12-05 13:42:52 +0800

    get all global components to import in main.js

---
+++ src/components/index.js
@@ -0,0 +1,11 @@
+import borderBox1 from './borderBox1/index'
+import borderBox2 from './borderBox2/index'
+import borderBox3 from './borderBox3/index'
+import borderBox4 from './borderBox4/index'
+
+export default function (Vue) {
+  Vue.component('borderBox1', borderBox1)
+  Vue.component('borderBox2', borderBox2)
+  Vue.component('borderBox3', borderBox3)
+  Vue.component('borderBox4', borderBox4)
+}
