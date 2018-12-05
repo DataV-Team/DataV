c583f7b2949f39756862ba1646abb2506eed5f66
jiaming <743192023@qq.com>
2018-12-05 18:30:45 +0800

    add some new components

--- src/components/index.js
+++ src/components/index.js
@@ -3,9 +3,22 @@ import borderBox2 from './borderBox2/index'
 import borderBox3 from './borderBox3/index'
 import borderBox4 from './borderBox4/index'
 
+import decoration1 from './decoration1/index'
+import decoration2 from './decoration2/index'
+
+import numberShow from './numberShow/index.vue'
+
+import capsuleChart from './capsuleChart/index.vue'
+import ringChart from './ringChart/index.vue'
+
 export default function (Vue) {
   Vue.component('borderBox1', borderBox1)
   Vue.component('borderBox2', borderBox2)
   Vue.component('borderBox3', borderBox3)
   Vue.component('borderBox4', borderBox4)
+  Vue.component('decoration1', decoration1)
+  Vue.component('decoration2', decoration2)
+  Vue.component('numberShow', numberShow)
+  Vue.component('capsuleChart', capsuleChart)
+  Vue.component('ringChart', ringChart)
 }
