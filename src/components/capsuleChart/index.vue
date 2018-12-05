0ce1209fb3086ded66737811bb585c40701ec3ef
jiaming <743192023@qq.com>
2018-12-05 18:31:36 +0800

    add capsule and ring chart

---
+++ src/components/capsuleChart/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="capsule-chart">
+    this is capsule chart
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CapsuleChart'
+}
+</script>
+
+<style lang="less">
+.capsule-chart {}
+</style>
---
+++ src/components/ringChart/index.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="ring-chart">
+    this is ring chart
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'RingChart'
+}
+</script>
+
+<style lang="less">
+.ring-chart {}
+</style>
