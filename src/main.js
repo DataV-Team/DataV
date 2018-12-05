b66f156e9691fc6eb5a7adc241933e453e9067e9
jiaming <743192023@qq.com>
2018-12-05 13:41:53 +0800

    import components and plugins

--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import './assets/style/index.less'
+
+import plugins from './plugins/index'
+
+import globalComponents from './components/index'
+
+Vue.use(plugins)
+
+Vue.use(globalComponents)
+
 Vue.config.productionTip = false
 
 new Vue({
