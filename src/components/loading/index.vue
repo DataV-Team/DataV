68d4877bc7cb184ccb26bcfef41face68b259855
jiaming <743192023@qq.com>
2018-12-07 15:49:45 +0800

    add loading component

---
+++ src/components/loading/index.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="loading">
+    <img class="loading-img" src="./img/loading.png">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Loading'
+}
+</script>
+
+<style lang="less">
+.loading {
+  position: absolute;
+  top: 0px;
+  left: 0px;
+  width: 100%;
+  height: 100%;
+
+  .loading-img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    width: 30px;
+    height: 30px;
+    margin-left: -15px;
+    margin-top: -15px;
+    transform: rotate(0deg);
+    animation: round 1s linear infinite;
+  }
+
+  @keyframes round {
+    to {
+      transform: rotate(360deg)
+    }
+  }
+}
+</style>
