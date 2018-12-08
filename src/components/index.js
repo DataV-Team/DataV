8fd59e41115d591a4c683956b8c61c4626a4b165
jiaming <743192023@qq.com>
2018-12-08 16:05:04 +0800

    add border box 6 & polyline chart

--- src/components/index.js
+++ src/components/index.js
@@ -3,6 +3,7 @@ import borderBox2 from './borderBox2/index'
 import borderBox3 from './borderBox3/index'
 import borderBox4 from './borderBox4/index'
 import borderBox5 from './borderBox5/index'
+import borderBox6 from './borderBox6/index'
 
 import decoration1 from './decoration1/index'
 import decoration2 from './decoration2/index'
@@ -12,6 +13,7 @@ import numberShow from './numberShow/index.vue'
 
 import capsuleChart from './capsuleChart/index.vue'
 import ringChart from './ringChart/index.vue'
+import polylineChart from './polylineChart/index.vue'
 
 import loading from './loading/index.vue'
 
@@ -21,11 +23,13 @@ export default function (Vue) {
   Vue.component('borderBox3', borderBox3)
   Vue.component('borderBox4', borderBox4)
   Vue.component('borderBox5', borderBox5)
+  Vue.component('borderBox6', borderBox6)
   Vue.component('decoration1', decoration1)
   Vue.component('decoration2', decoration2)
   Vue.component('decoration3', decoration3)
   Vue.component('numberShow', numberShow)
   Vue.component('capsuleChart', capsuleChart)
+  Vue.component('polylineChart', polylineChart)
   Vue.component('ringChart', ringChart)
   Vue.component('loading', loading)
 }
---
+++ src/components/polylineChart/index.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="polyline-chart">
+    <canvas :ref="ref" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'PolylineChart',
+  props: ['data'],
+  data () {
+    return {
+      ref: `ring-chart-${(new Date()).getTime()}`,
+      canvasDom: '',
+      canvasWH: [0, 0],
+      ctx: ''
+    }
+  },
+  methods: {
+    init () {
+      const { $nextTick, initCanvas } = this
+
+      $nextTick(e => {
+        initCanvas()
+      })
+    },
+    initCanvas () {
+      const { $refs, ref, canvasWH } = this
+
+      const canvas = this.canvasDom = $refs[ref]
+
+      canvasWH[0] = canvas.clientWidth
+      canvasWH[1] = canvas.clientHeight
+
+      canvas.setAttribute('width', canvasWH[0])
+      canvas.setAttribute('height', canvasWH[1])
+
+      this.ctx = canvas.getContext('2d')
+    }
+  },
+  mounted () {
+    const { init } = this
+
+    init()
+  }
+}
+</script>
+
+<style>
+
+</style>
