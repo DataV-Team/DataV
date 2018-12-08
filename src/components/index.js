087dbeae23b8e874d2cf86a91c89d143103b3329
jiaming <743192023@qq.com>
2018-12-08 19:06:00 +0800

    add new components import in global

--- src/components/index.js
+++ src/components/index.js
@@ -8,12 +8,15 @@ import borderBox6 from './borderBox6/index'
 import decoration1 from './decoration1/index'
 import decoration2 from './decoration2/index'
 import decoration3 from './decoration3/index'
-
-import numberShow from './numberShow/index.vue'
+import decoration4 from './decoration4/index'
 
 import capsuleChart from './capsuleChart/index.vue'
 import ringChart from './ringChart/index.vue'
 import polylineChart from './polylineChart/index.vue'
+
+import numberShow from './numberShow/index.vue'
+
+import scrollBoard from './scrollBoard/index.vue'
 
 import loading from './loading/index.vue'
 
@@ -27,9 +30,11 @@ export default function (Vue) {
   Vue.component('decoration1', decoration1)
   Vue.component('decoration2', decoration2)
   Vue.component('decoration3', decoration3)
-  Vue.component('numberShow', numberShow)
+  Vue.component('decoration4', decoration4)
   Vue.component('capsuleChart', capsuleChart)
   Vue.component('polylineChart', polylineChart)
   Vue.component('ringChart', ringChart)
+  Vue.component('numberShow', numberShow)
+  Vue.component('scrollBoard', scrollBoard)
   Vue.component('loading', loading)
 }
