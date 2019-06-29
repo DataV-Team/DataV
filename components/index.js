addf491d2baa0261db97fa0400f4f5de48be0c77
jiaming743 <743192023@qq.com>
2019-06-29 19:51:13 +0800

    add new component

--- components/index.js
+++ components/index.js
@@ -10,6 +10,7 @@ import borderBox5 from './borderBox5/index'
 import borderBox6 from './borderBox6/index'
 import borderBox7 from './borderBox7/index'
 import borderBox8 from './borderBox8/index'
+import borderBox9 from './borderBox9/index'
 
 // decoration
 import decoration1 from './decoration1/index'
@@ -20,6 +21,8 @@ import decoration5 from './decoration5/index'
 import decoration6 from './decoration6/index'
 import decoration7 from './decoration7/index'
 import decoration8 from './decoration8/index'
+import decoration9 from './decoration9/index'
+import decoration10 from './decoration10/index'
 
 // charts
 import charts from './charts/index.vue'
@@ -47,6 +50,7 @@ export default function (Vue) {
   Vue.component('dvBorderBox6', borderBox6)
   Vue.component('dvBorderBox7', borderBox7)
   Vue.component('dvBorderBox8', borderBox8)
+  Vue.component('dvBorderBox9', borderBox9)
 
   // decoration
   Vue.component('dvDecoration1', decoration1)
@@ -57,6 +61,8 @@ export default function (Vue) {
   Vue.component('dvDecoration6', decoration6)
   Vue.component('dvDecoration7', decoration7)
   Vue.component('dvDecoration8', decoration8)
+  Vue.component('dvDecoration9', decoration9)
+  Vue.component('dvDecoration10', decoration10)
 
   // charts
   Vue.component('dvCharts', charts)
