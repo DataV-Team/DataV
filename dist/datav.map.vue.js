9318431c88a180d9626b61b113c444f32db71af2
jiaming743 <743192023@qq.com>
2020-07-02 11:15:51 +0800

    update dist and lib

--- dist/datav.map.vue.js
+++ dist/datav.map.vue.js
@@ -38,8 +38,8 @@
     const minusY = Math.abs(pointOne[1] - pointTwo[1]);
     return Math.sqrt(minusX * minusX + minusY * minusY);
   }
-  function uuid() {
-    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+  function uuid(hasHyphen) {
+    return (hasHyphen ? 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx' : 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx').replace(/[xy]/g, function (c) {
       const r = Math.random() * 16 | 0;
       const v = c == 'x' ? r : r & 0x3 | 0x8;
       return v.toString(16);
